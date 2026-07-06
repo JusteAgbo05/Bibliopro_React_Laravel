83c27c6b2df1443c888c971c7d95c69da067ae36
Juste Agbo <vivienagbo56@gmail.com>
2026-07-07 00:43:29 +0100

    fix: force le peuplement des donnees avec les seeders

--- bibliopro-api/entrypoint.sh
+++ bibliopro-api/entrypoint.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
 # Exécuter les migrations automatiquement
-php artisan migrate --force --seed
+php artisan migrate: --seed --force
 
 # Lancer le serveur Apache en arrière-plan (commande par défaut de l'image PHP)
 apache2-foreground
