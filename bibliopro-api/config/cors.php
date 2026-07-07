efc5f70ba13241693043e8266300da69744ece85
Juste Agbo <vivienagbo56@gmail.com>
2026-07-07 06:40:50 +0100

    fix mise à jour lien vercel

--- bibliopro-api/config/cors.php
+++ bibliopro-api/config/cors.php
@@ -19,7 +19,7 @@ return [
 
     'allowed_methods' => ['*'],
 
-    'allowed_origins' => ['hhttps://bibliopro-react-laravel.vercel.app/'],
+    'allowed_origins' => ['https://bibliopro-react-laravel.vercel.app/'],
 
     'allowed_origins_patterns' => [],
 
