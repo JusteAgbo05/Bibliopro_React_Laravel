fe2a5072446e5451aacd0e2c8b9da27e8c22d561
Juste Agbo <vivienagbo56@gmail.com>
2026-07-07 07:30:27 +0100

    fix: lien storage mis à jour

--- bibliopro-api/entrypoint.sh
+++ bibliopro-api/entrypoint.sh
@@ -2,6 +2,7 @@
 
 # Exécuter les migrations automatiquement
 php artisan migrate: --seed --force
+php artisan storage:link
 
 # Lancer le serveur Apache en arrière-plan (commande par défaut de l'image PHP)
 apache2-foreground
